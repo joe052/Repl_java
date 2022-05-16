e5af6e8c8dc838065f7e020add620132b9216c5d
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-05-16 15:55:31 +0000

    modified

--- Math.java
+++ Math.java
@@ -17,47 +17,39 @@ public class Math{
     return x+y;
   }
 
-  public static void lonelyinteger(List<Integer> a) {
+  public static int lonelyinteger(List<Integer> a) {
     // Write your code here
         Collections.sort(a);
         int [] arr = new int[a.size()+1];
-        //int [] idx = new int[a.size()];
-        //int y = 0;
         int t = 1;
         int test = 0;
         int n = 0;
-         
+
+        //pushing elements to my new array
         for(int i = 0;i < a.size(); i++){
             int x = a.get(i);
             arr[i] = x;
         }
-        
+
+        //checking for duplicates using int test as a control
         for(int i = 0;i < a.size(); i++){
             if(arr[i] == arr[t]){
-                //idx[y] = i;
-                //y ++;
                 t++;
                 test = 0;
             }else{
               test++;
               if(test == 2){
-              n = i;
+                n = i;
               }
               t++;
             }
         }
 
-        if(n == 0){
-          
-        }
-
-        int res = arr[n];
+        int result = arr[n];
         
         System.out.println(Arrays.toString(arr));
-        //System.out.println(Arrays.toString(idx));
-        System.out.println(res);
-        //return arr[arr.length - 1];
-        //return idx;
-        
+        System.out.println(result);
+    
+        return result;        
     }
 }
