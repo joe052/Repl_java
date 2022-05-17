b227475c91758b0de81a67a5a52cef2506152ceb
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-05-17 16:15:44 +0000

    modifications

--- Arrays.java
+++ Arrays.java
@@ -4,7 +4,9 @@ public class Arrays{
   int [][] test = new int[5][3];
   
   public void check(){
-    int [][] myArr = {{50,45,67,89,77},{33,44,67,87,74},{90,87,78,69,80}};
+    int [][] myArr = {{50,45,67},{33,44,67},{90,87,78}};
+    int[]testArr = new int[3];
+    int[]testArr2 = new int[3];
 
     //System.out.println(marks[0][3]);
     for(int i = 0;i < myArr.length;i++){
@@ -12,33 +14,32 @@ public class Arrays{
         System.out.println(myArr[i][j]);
       }
       System.out.println();
+    }  
+
+    //establishing the right side diagonal
+    for(int i = 0;i < myArr.length;i++){
+      for(int j = 0;j < myArr[i].length;j++){
+        if(j == i){
+          testArr[i] = myArr[i][j];
+        }
+      }
+    }  
+
+    //establishing the left side diagonal
+    for(int i = myArr.length - 1;i >= 0;i--){
+      for(int j = myArr[i].length - 1;j >= 0;j--){
+        if(j == i){
+          testArr2[i] = myArr[i][j];
+        }
+      }
+    }  
+
+    for(int i=0; i < testArr.length; i++){
+     System.out.println(testArr[i]);
     }
 
-    int[][]myArr = new int[3][3];
-        //arr.toArray(myArr);
-        
-        for(int i = 0;i < myArr.length;i++){
-            for(int j = 0;j < myArr[i].length;j++){
-                //System.out.println(myArr[i][j] + "\t");
-                myArr[i][j] = arr.get(i).get(j);
-            }
-        }
-        
-        int tLft = myArr[0][0];
-        int ctr = myArr[1][1];
-        int bRgt = myArr[2][2];
-        int bLft = myArr[2][0];
-        int tRgt = myArr[0][2];
-        
-        int diag1 = tLft + ctr + bRgt;
-        int diag2 = tRgt + ctr + bLft;
-        
-        int result = diag1 - diag2;
-        if(result < 0){
-            result = - result;
-        }
-        return result;
+    for(int i=0; i < testArr2.length; i++){
+     System.out.println(testArr2[i]);
     }
-
-  }
+  }  
 }
