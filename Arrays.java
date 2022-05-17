15350c828d2519830fbcca7fb715d3d55d60586e
Joe Karanja <87515353+joe052@users.noreply.github.com>
2022-05-17 16:48:19 +0000

    completed the check function

--- Arrays.java
+++ Arrays.java
@@ -4,11 +4,10 @@ public class Arrays{
   int [][] test = new int[5][3];
   
   public void check(){
-    int [][] myArr = {{50,45,67},{33,44,67},{90,87,78}};
-    int[]testArr = new int[3];
-    int[]testArr2 = new int[3];
+    int [][] myArr = {{50,45,67,70},{33,44,58,64},{90,87,78,82},{68,72,80,79}};
+    int[]testArr = new int[myArr.length];
+    int[]testArr2 = new int[myArr.length];
 
-    //System.out.println(marks[0][3]);
     for(int i = 0;i < myArr.length;i++){
       for(int j = 0;j < myArr[i].length;j++){
         System.out.println(myArr[i][j]);
@@ -26,20 +25,44 @@ public class Arrays{
     }  
 
     //establishing the left side diagonal
-    for(int i = myArr.length - 1;i >= 0;i--){
+    for(int i = 0;i < myArr.length;i++){
       for(int j = myArr[i].length - 1;j >= 0;j--){
-        if(j == i){
+        if(i+j == myArr.length - 1){
           testArr2[i] = myArr[i][j];
         }
       }
     }  
 
+    //adding elements of first diagonal
+    int diag1 = 0;
+    for(int i=0; i < testArr.length; i++){
+      diag1 = testArr[i] + diag1;
+    }
+
+    //adding elements of second diagonal
+    int diag2 = 0;
+    for(int i=0; i < testArr2.length; i++){
+      diag2 = testArr2[i] + diag2;
+    }
+
+    //performing the diagonal difference
+    int result = diag1-diag2;
+    if(result < 0){
+      result = -result;
+    }
+    
+    //printing first diagonal
     for(int i=0; i < testArr.length; i++){
      System.out.println(testArr[i]);
     }
 
+    //printing second diagonal
     for(int i=0; i < testArr2.length; i++){
      System.out.println(testArr2[i]);
     }
+
+    System.out.println("sum is "+diag1);
+    System.out.println("sum is "+diag2);
+    System.out.println("difference is "+result);
   }  
 }
